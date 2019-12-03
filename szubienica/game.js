cea0173a4598e633100078052f11ec335b45cdc5
Marcin Kałużny <marcin.kaluzny@codeme.pl>
2019-12-03 19:53:24 +0100

    zaj

--- szubienica/game.js
+++ szubienica/game.js
@@ -1,3 +1,26 @@
-(function (a, b) {
-  console.log(a + b);
-}(3, 6))
+(function () {
+  const slowa = ['javascript', 'java', 'php', 'elm'];
+  const index = Math.floor(Math.random() * slowa.length);
+  const slowo = slowa[index];
+  let ukryteSlowo = Array.from(slowo, () => '_');
+  let liczbaBledow = 5;
+
+  //alert('szukane słowo: ' + ukryteSlowo.join(''));
+
+  //const wpisanaLitera = prompt('Wpisz litere');
+  while (ukryteSlowo.join('') !== slowo) {
+    alert('Slowo: ' + ukryteSlowo.join(''));
+    const wpisanaLitera = prompt('Wpisz litere');
+
+    ukryteSlowo = Array.from(
+      slowo,
+      (litera, index) => litera === wpisanaLitera ? litera : ukryteSlowo[index]
+    );
+
+
+    liczbaBledow -= 1;
+  }
+
+  console.log(wpisanaLitera);
+}());
+
