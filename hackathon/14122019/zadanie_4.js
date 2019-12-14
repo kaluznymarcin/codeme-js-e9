7ebcd377d173a9cb8d5c822c8c716c19147eedde
Marcin Kałużny <marcin.kaluzny@codeme.pl>
2019-12-14 14:13:16 +0100

    zad4

---
+++ hackathon/14122019/zadanie_4.js
@@ -0,0 +1,10 @@
+/**
+ * Zapisz wycinek HTML jako obiekt javascript
+ */
+
+/*
+<article class="box" role="listitem">
+    <h2 class="box__title">Tytul</h2>
+    <div class="box__content">jakis tekst</div>
+</article >
+*/
