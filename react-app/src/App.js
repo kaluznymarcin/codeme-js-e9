1954d7853e700884dc10bc6216a7f7e2e064e252
Marcin Kałużny <marcin.kaluzny@codeme.pl>
2020-01-28 18:20:51 +0100

    react-app

---
+++ react-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import logo from './logo.svg';
+import React from 'react';
+
+function App() {
+  const STYLE = {fontWeight: 'bold', fontSize: '40px'}
+  return (
+    <div className="App">
+      <span style={STYLE}>hfshfsdhfh</span>
+    </div>
+  );
+}
+
+export default App;
