f8a942c866ce56702933c5d1e5c53ce6c08db499
Marcin Kałużny <marcin.kaluzny@codeme.pl>
2020-02-11 15:58:23 +0100

    przygotowanie

---
+++ react-app/src/constants.js
@@ -0,0 +1,7 @@
+export const TODAY_EXCHANGERATES = 'http://api.nbp.pl/api/exchangerates/tables/a/today/';
+
+export const LAST_FIVE_DAYS_EXCHANGERATES = 'http://api.nbp.pl/api/exchangerates/tables/a/last/5/';
+
+export const GOLD_TODAY = 'http://api.nbp.pl/api/cenyzlota/today/';
+
+export const LAST_FIVE_DAYS_GOLD = 'http://api.nbp.pl/api/cenyzlota/last/5/';
